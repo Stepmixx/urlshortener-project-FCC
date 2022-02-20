9795f418794cbc6a11ea128ee599acba9550e68b
Stephan <stepmix123@gmail.com>
2022-02-19 21:00:54 -0400

    url shortener endpoints

---
+++ routes/api.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const ShortUrl = require("../models/shortUrl");
+const router = express.Router();
+
+//Check if the given url exist and returns the shortenUrl from DB, else creates one and save it if the given url is valid
+router.post("/shorturl", findByOriginalUrl, async (req, res) => {
+  const { url, shortUrl } = res;
+
+  if (shortUrl) {
+    res.json(shortUrl);
+    return;
+  }
+
+  try {
+    const newUrl = new URL(url);
+    const newShortUrl = await ShortUrl.create({ original_url: newUrl });
+    const { original_url, short_url } = await newShortUrl.save();
+    res.status(201).json({ original_url, short_url });
+  } catch (error) {
+    res.status(400).json({ message: "invalid url" });
+  }
+});
+
+//Find in Db the given shortUrl from params and redirect to the originalUrl
+router.get("/shorturl/:shortUrl", async (req, res) => {
+  const short_url = req.params.shortUrl;
+  if (short_url) {
+    try {
+      const { original_url } = await ShortUrl.findOne({ short_url });
+      res.redirect(original_url);
+    } catch (err) {
+      res.status(400).json({ message: "this url doesn't exist" });
+    }
+  }
+});
+
+//Find and return shortUrl by given url, else returns the same url
+async function findByOriginalUrl(req, res, next) {
+  const url = req.body.url;
+
+  try {
+    const { original_url, short_url } = await ShortUrl.findOne({
+      original_url: url,
+    });
+    res.shortUrl = { original_url, short_url };
+  } catch (err) {
+    res.url = url;
+  }
+
+  next();
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const apiRouter = require("./routes/api");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+// Conection to database
+const uri = process.env.MONGO_URI;
+const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(uri, connectionOptions);
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.use("/api", apiRouter);
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
